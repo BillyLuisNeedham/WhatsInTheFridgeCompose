14fcfc99be316c94df86a6f4a2975331789a60a8
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-30 22:37:51 +0100

    FoodCard now has default modifier

--- app/src/main/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodListComponents.kt
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodListComponents.kt
@@ -2,9 +2,10 @@ package com.billyluisneedham.whatsinthefridge.ui.foodlist
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.billyluisneedham.whatsinthefridge.model.FoodInstance
 
 @Composable
-fun FoodCard(food: FoodInstance) {
+fun FoodCard(modifier: Modifier = Modifier, food: FoodInstance) {
     Text(text = food.foodType.name)
 }
