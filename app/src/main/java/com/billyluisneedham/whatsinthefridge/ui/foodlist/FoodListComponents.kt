4fccf7983b2384c2408bf9988e33a12ea21602eb
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-30 22:36:02 +0100

    FoodCard displays food name

--- app/src/main/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodListComponents.kt
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodListComponents.kt
@@ -1,9 +1,10 @@
 package com.billyluisneedham.whatsinthefridge.ui.foodlist
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.billyluisneedham.whatsinthefridge.model.FoodInstance
 
 @Composable
 fun FoodCard(food: FoodInstance) {
-
+    Text(text = food.foodType.name)
 }
