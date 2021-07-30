3b945e366e19071f0e4911f5cf8b49f46ee1ccd4
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-30 20:01:08 +0100

    Hilt added to project

--- app/src/main/java/com/billyluisneedham/whatsinthefridge/MainActivity.kt
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/MainActivity.kt
@@ -9,8 +9,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.billyluisneedham.whatsinthefridge.ui.theme.WhatsInTheFridgeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
