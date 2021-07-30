678a106fde6b94c3019d0602701fc67912951b9a
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-30 22:33:30 +0100

    FoodCard build out

--- app/src/androidTest/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodCardTest.kt
+++ app/src/androidTest/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodCardTest.kt
@@ -1,17 +1,30 @@
 package com.billyluisneedham.whatsinthefridge.ui.foodlist
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import org.junit.Assert.*
+import androidx.compose.ui.test.onNodeWithText
+import com.billyluisneedham.whatsinthefridge.mocks.FoodMocks
+import com.billyluisneedham.whatsinthefridge.ui.theme.WhatsInTheFridgeTheme
 import org.junit.Rule
 import org.junit.Test
 
 class FoodCardTest {
 
+    private val mockFoodInstance = FoodMocks.foodInstanceMock
+
     @get:Rule
     val compostTestRule = createComposeRule()
 
     @Test
-    fun foodCardDisplaysNameOfFood() {
+    fun foodCardDisplaysNameByDefault() {
+        compostTestRule.setContent {
+            WhatsInTheFridgeTheme {
+                FoodCard(
+                    food = mockFoodInstance
+                )
+            }
+        }
 
+        compostTestRule.onNodeWithText(text = mockFoodInstance.foodType.name).assertIsDisplayed()
     }
 }
--- app/src/main/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodListComponents.kt
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/ui/foodlist/FoodListComponents.kt
@@ -1,8 +1,9 @@
 package com.billyluisneedham.whatsinthefridge.ui.foodlist
 
 import androidx.compose.runtime.Composable
+import com.billyluisneedham.whatsinthefridge.model.FoodInstance
 
 @Composable
-fun FoodCard() {
+fun FoodCard(food: FoodInstance) {
 
 }
