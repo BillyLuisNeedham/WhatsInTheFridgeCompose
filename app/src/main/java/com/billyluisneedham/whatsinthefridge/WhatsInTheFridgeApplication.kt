154d916cb2f4f12a89830b6bc11ca84b2163a418
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-30 19:25:11 +0100

    WhatsInTheFridgeApplication.kt

---
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/WhatsInTheFridgeApplication.kt
@@ -0,0 +1,7 @@
+package com.billyluisneedham.whatsinthefridge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WhatsInTheFridgeApplication: Application()
