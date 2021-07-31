b6a5ed990f9f2ea6a38c3b30c4d79d8a7fde5a77
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-31 01:48:46 +0100

    TimeDisplayerImpl.kt build out WIP

--- app/src/main/java/com/billyluisneedham/whatsinthefridge/util/timedisplayer/TimeDisplayerImpl.kt
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/util/timedisplayer/TimeDisplayerImpl.kt
@@ -1,6 +1,7 @@
 package com.billyluisneedham.whatsinthefridge.util.timedisplayer
 
 import android.content.Context
+import android.util.Log
 
 object TimeDisplayerImpl : TimeDisplayer {
     override fun getTimeDifferenceMessage(
@@ -8,7 +9,17 @@ object TimeDisplayerImpl : TimeDisplayer {
         firstTime: Long,
         secondTime: Long
     ): String {
-        //TODO WRITE
+        //create a list of firstTime secondTime
+        val timeDifference = listOf(firstTime, secondTime).also {
+            it.sorted()
+
+            //TODO REMOVE WHEN DONE TESTING
+            Log.d()
+        }
+        //sort list into order of smallest to largest
+        //get result of list[1] - list[0]
+        //if result is < 10 seconds return now string
+
         return ""
     }
 }
