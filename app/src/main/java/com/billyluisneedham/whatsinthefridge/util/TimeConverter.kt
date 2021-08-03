f0af46111921a55de49c061d0d12d320633f9feb
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-08-03 20:56:14 +0100

    TimeCoverter now converting set times to milliSeconds

--- app/src/main/java/com/billyluisneedham/whatsinthefridge/util/TimeConverter.kt
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/util/TimeConverter.kt
@@ -19,3 +19,23 @@ fun Long.millisecondsToDays(): Long {
 fun Long.millisecondsToWeeks(): Long {
     return this / 1000 / 60 / 60/ 24 / 7
 }
+
+fun Long.secondsToMilliseconds(): Long {
+    return this * 1000
+}
+
+fun Long.minutesToMilliseconds(): Long {
+    return this * 1000 * 60
+}
+
+fun Long.hoursToMilliseconds(): Long {
+    return this * 1000 * 60 * 60
+}
+
+fun Long.daysToMilliseconds(): Long {
+    return this * 1000 * 60 * 60 * 24
+}
+
+fun Long.weeksToMilliseconds(): Long {
+    return this * 1000 * 60 * 60 * 24 * 7
+}
--- app/src/test/java/com/billyluisneedham/whatsinthefridge/util/TimeConverterKtTest.kt
+++ app/src/test/java/com/billyluisneedham/whatsinthefridge/util/TimeConverterKtTest.kt
@@ -6,10 +6,16 @@ import org.junit.Test
 
 class TimeConverterKtTest {
 
+    private val oneSecondInMilliseconds = 1000L
+    private val oneMinuteInMilliSeconds = 1000L * 60L
+    private val oneHourInMilliSeconds = 1000L * 60L * 60
+    private val oneDayInMilliSeconds= 1000L * 60L * 60 * 24L
+    private val oneWeekInMilliSeconds = 1000 * 60L * 60 * 24 * 7
+
+
+
     @Test
     fun millisecondsToSeconds_convertedCorrectly_correctValueReturned() {
-        val oneSecondInMilliseconds = 1000L
-
         val result = oneSecondInMilliseconds.millisecondsToSeconds()
 
         assertThat(result, `is`(1))
@@ -17,8 +23,6 @@ class TimeConverterKtTest {
 
     @Test
     fun millisecondsToMinutes_convertedCorrectly_correctValueReturned() {
-        val oneMinuteInMilliSeconds = 1000L * 60L
-
         val result = oneMinuteInMilliSeconds.millisecondsToMinutes()
 
         assertThat(result, `is`(1))
@@ -26,8 +30,6 @@ class TimeConverterKtTest {
 
     @Test
     fun millisecondsToHours_convertedCorrectly_correctValueReturned() {
-        val oneHourInMilliSeconds = 1000L * 60L * 60
-
         val result = oneHourInMilliSeconds.millisecondsToHours()
 
         assertThat(result, `is`(1))
@@ -35,8 +37,6 @@ class TimeConverterKtTest {
 
     @Test
     fun millisecondsToDays_convertedCorrectly_correctValueReturned() {
-        val oneDayInMilliSeconds= 1000L * 60L * 60 * 24L
-
         val result = oneDayInMilliSeconds.millisecondsToDays()
 
         assertThat(result, `is`(1))
@@ -44,11 +44,44 @@ class TimeConverterKtTest {
 
     @Test
     fun millisecondsToWeeks_convertedCorrectly_correctValueReturned() {
-        val oneWeekInMilliSeconds = 1000 * 60L * 60 * 24 * 7
-
         val result = oneWeekInMilliSeconds.millisecondsToWeeks()
 
         assertThat(result, `is`(1))
+    }
+
+    @Test
+    fun secondsToMilliseconds_convertedCorrectly_correctValueReturned() {
+        val result = 1L.secondsToMilliseconds()
+
+        assertThat(result, `is`(oneSecondInMilliseconds))
+    }
+
+    @Test
+    fun minutesToMilliseconds_convertedCorrectly_correctValueReturned() {
+        val result = 1L.minutesToMilliseconds()
+
+        assertThat(result, `is`(oneMinuteInMilliSeconds))
+    }
+
+    @Test
+    fun hoursToMilliseconds_convertedCorrectly_correctValueReturned() {
+        val result = 1L.hoursToMilliseconds()
+
+        assertThat(result, `is`(oneHourInMilliSeconds))
+    }
+
+    @Test
+    fun daysToMilliseconds_convertedCorrectly_correctValueReturned() {
+        val result = 1L.daysToMilliseconds()
+
+        assertThat(result, `is`(oneDayInMilliSeconds))
+    }
+
+    @Test
+    fun weeksToMilliseconds_convertedCorrectly_correctValueReturned() {
+        val result = 1L.weeksToMilliseconds()
+
+        assertThat(result, `is`(oneWeekInMilliSeconds))
     }
 
 }
