8d7d485eb2e18ed76c57e0193ef5abaa098e31eb
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-08-09 21:28:45 +0100

    FoodInstanceDataSource now returns flow

--- app/src/main/java/com/billyluisneedham/whatsinthefridge/data/source/FoodInstanceDataSource.kt
+++ app/src/main/java/com/billyluisneedham/whatsinthefridge/data/source/FoodInstanceDataSource.kt
@@ -1,11 +1,12 @@
 package com.billyluisneedham.whatsinthefridge.data.source
 
 import com.billyluisneedham.whatsinthefridge.domain.model.FoodInstance
+import kotlinx.coroutines.flow.Flow
 
 interface FoodInstanceDataSource {
 
     suspend fun add(foodInstance: FoodInstance)
-    suspend fun getAll(foodInstance: FoodInstance): List<FoodInstance>
+    fun getAll(foodInstance: FoodInstance): Flow<List<FoodInstance>>
     suspend fun delete(foodInstance: FoodInstance)
     suspend fun update(foodInstance: FoodInstance)
 
